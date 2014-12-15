7c430e0ce27f32ca7fc8d5cb61e85de6af401131
Wes Alcock <bluebery@gmail.com>
2014-12-15 12:41:01 -0800

    initial commit for repo
    basic api functions, no validation yet

---
+++ app/models/gamestat.js
@@ -0,0 +1,10 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GameStatSchema = new Schema({
+	name: String, // stat name
+	value: Number, // stat value
+	uid: Number // user id
+});
+
+module.exports = mongoose.model('GameStat', GameStatSchema);
---
+++ server.js
@@ -0,0 +1,99 @@
+﻿// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to our database
+mongoose.connect('mongodb://192.168.1.88:27017/test'); 
+
+// configure app to use bodyParser() - this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set our port
+var port = process.env.PORT || 8080; 
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); 
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function (req, res) {
+	res.json({ message: 'welcome to the LDRLY api' });
+});
+
+// on routes that end in /sendStat
+// ----------------------------------------------------
+router.route('/sendStat')
+
+	// create a game stat (accessed at POST http://localhost:8080/api/sendStat)
+	.post(function (req, res) {
+	
+		var gameStat = new GameStat(); 		// create a new instance of the Stat model
+		gameStat.name = req.body.name;  // set the stats name (comes from the request)
+		gameStat.value = req.body.value;
+		gameStat.uid = req.body.uid;
+	
+		// save the stat and check for errors
+		gameStat.save(function (err) {
+			if (err)
+				res.send(err);
+		
+			res.json({ message: 'GameStat created!' });
+		});
+	});
+
+// on routes that end in /getLeaderboard/:statName
+// ----------------------------------------------------
+router.route('/getLeaderboard/:statName')
+
+	// get the game stat with that id (accessed at GET http://localhost:8080/api/getLeaderboard/:statName)
+	.get(function (req, res) {
+		GameStat.find({ name: req.params.statName }).sort({ value: 'desc' }).lean().select('value uid -_id').exec(function (err, docs) {
+			if (err)
+				res.send(err);
+		
+			// now the docs are sorted, add the ranking property to each object
+			// we are able to do this because we used the lean() function on our query
+			for (var i = 0; i < docs.length; i++) {
+				docs[i].ranking = i + 1;
+			}
+		
+			res.json(docs);
+		});
+	});
+
+// on routes that end in /getStats/:uid
+// ----------------------------------------------------
+router.route('/getStats/:uid')
+
+	// get the game stat with that id (accessed at GET http://localhost:8080/api/getStats/:uid)
+	.get(function (req, res) {
+		GameStat.find({ uid: req.params.uid }).select('name value -_id').exec(function (err, docs) {
+			if (err)
+				res.send(err);
+		
+			res.json(docs);
+		});
+	});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('LDRLY RESTful API Started on Port ' + port + '. Created by Wes Alcock.');
+
+var GameStat = require('./app/models/gamestat');
